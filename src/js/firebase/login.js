2f4ff4284aae11bc87e453a3c5f54d1235cd0fdd
Jeek-p <61310684+Jeek-p@users.noreply.github.com>
2021-11-27 02:34:03 +0100

    Added email log in

--- src/js/firebase/login.js
+++ src/js/firebase/login.js
@@ -4,11 +4,8 @@ var uiConfig = {
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        //firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+
     ],
     // Terms of service url where to go
     tosUrl: 'index.html'
