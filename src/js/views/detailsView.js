f8714f4154e5520771666295061fdf97e39e26f0
Erik <erivin@kth.se>
2021-12-02 21:30:19 +0100

    Title date now only displaying year-format.

--- src/js/views/detailsView.js
+++ src/js/views/detailsView.js
@@ -1,25 +1,21 @@
 function DetailsView(props){
     return(
         <div>
-            <h2>{props.title.title} ({props.title.release_date})</h2>
+            <h2>{props.title.title} ({props.title.release_date.split('-')[0]})</h2>
             <div>
-                <img src={"https://image.tmdb.org/t/p/w500/" + (props.title.profile_path || props.title.poster_path)} height={250}/>
+                <img src={"https://image.tmdb.org/t/p/w500/" + props.title.poster_path} height={250}/>
             </div>
             <div class = "overview">
                 {props.title.overview}
                 
             </div>
             <div class = "rating">
-                Rating: {props.title.vote_average}/10
+                Rating: {(props.title.vote_average || "unrated")}
             </div>
-            <div class = "smallBorder">
-
-            </div>
-            <div>
+            <div class="buttons">
                 <button>Add to watchlist!</button>
                 <button>Cancel</button>
             </div>
         </div>
     );
 }
-
