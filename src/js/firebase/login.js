93402c2dca611437bfc271be763f5c4abcd14e7c
Jeek-p <61310684+Jeek-p@users.noreply.github.com>
2021-12-06 16:01:41 +0100

    Moved and updated public

--- src/js/firebase/login.js
+++ src/js/firebase/login.js
@@ -5,7 +5,7 @@ var uiConfig = {
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+       // firebase.auth.EmailAuthProvider.PROVIDER_ID,   **not working yet**
 
     ],
     // Terms of service url where to go
